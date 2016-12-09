8400214028e967d9dcb2cadaeb159e5daa698007
Zhu Jun <jun.zhu@vungle.com>
2016-12-09 14:35:55 +0800

    go example

---
+++ go/gobyexample/23_channels.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	go func() { messages <- "ping" } ()
+	go func() { messages <- "hello" } ()
+
+	msg1,msg2 := <- messages, <-messages
+	fmt.Println(msg1)
+	fmt.Println(msg2)
+}
