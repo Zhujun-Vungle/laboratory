95de514a43f125513aac0eec66824ac503e262ce
Zhujun-Vungle <jun.zhu@gmail.com>
2020-05-04 22:39:54 +0800

    self define optimizer

--- spark_extensioons/src/main/scala/SparkExtension.scala
+++ spark_extensioons/src/main/scala/SparkExtension.scala
@@ -1,5 +1,4 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.rules.Rule
 
@@ -11,14 +10,23 @@ object SparkExtension extends App {
       .appName("Jun-test")
 
 
-    case class MyRule(spark: SparkSession) extends Rule[LogicalPlan] {
-      override def apply(plan: LogicalPlan): LogicalPlan = plan
+//    case class MyRule(spark: SparkSession) extends Rule[LogicalPlan] {
+//      override def apply(plan: LogicalPlan): LogicalPlan = plan
+//    }
+    //    spark.withExtensions(_.injectParser((_, _) => CatalystSqlParser))
+
+    case class PrintRule(spark: SparkSession) extends Rule[LogicalPlan] {
+      override val ruleName: String = "Optimize printer"
+      override def apply(plan: LogicalPlan): LogicalPlan = {
+        println(plan.verboseStringWithSuffix)
+        plan
+      }
     }
 
-    spark.withExtensions(_.injectParser((_, _) => CatalystSqlParser))
-    val s = spark.getOrCreate()
-    println(s.sessionState.sqlParser)
 
+    spark.withExtensions(_.injectOptimizerRule(PrintRule))
+    val s = spark.getOrCreate()
+    s.sql("show tables").show(100, false)
     s.stop()
   }
 }
